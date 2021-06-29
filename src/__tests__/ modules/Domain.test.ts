27bae70bf2cbe8705da5db7abbad73116b8255e7
IARKI <iarki90@gmail.com>
2021-06-29 10:01:09 -0400

    feat: domain module

---
+++ src/__tests__/ modules/Domain.test.ts
@@ -0,0 +1,36 @@
+import { Message } from "../../modules/Message.module";
+import * as nock from "nock";
+import { Domain } from "../../modules/Domain.module";
+
+describe("Message Module", () => {
+  const domainModule = new Domain("test_key", "http://test.com");
+  it("list", async () => {
+    const params = { limit: 20, page: 2, verified: true };
+    nock("http://test.com")
+      .get("/domains")
+      .query(params)
+      .reply(200, { key1: "key1_value" }, { header1: "test" });
+    const getDomains = await domainModule.list(params);
+    expect(getDomains.headers).toMatchObject({ header1: "test", "content-type": "application/json" });
+    expect(getDomains.body).toMatchObject({ key1: "key1_value" });
+    expect(getDomains.statusCode).toBe(200);
+  });
+  it("recipients", async () => {
+    const params = { limit: 20, page: 2 };
+    nock("http://test.com")
+      .get("/domains/test_id/recipients")
+      .query(params)
+      .reply(200, { key1: "key1_value" }, { header1: "test" });
+    const getDomains = await domainModule.recipients("test_id", params);
+    expect(getDomains.headers).toMatchObject({ header1: "test", "content-type": "application/json" });
+    expect(getDomains.body).toMatchObject({ key1: "key1_value" });
+    expect(getDomains.statusCode).toBe(200);
+  });
+  it("single", async () => {
+    nock("http://test.com").get("/domains/test_id").reply(200, { key1: "key1_value" }, { header1: "test" });
+    const getDomain = await domainModule.single("test_id");
+    expect(getDomain.headers).toMatchObject({ header1: "test", "content-type": "application/json" });
+    expect(getDomain.body).toMatchObject({ key1: "key1_value" });
+    expect(getDomain.statusCode).toBe(200);
+  });
+});
---
+++ src/modules/Domain.module.ts
@@ -0,0 +1,29 @@
+import { RequestService } from "../services/request.service";
+import { APIResponse } from "./MailerSend.module";
+import { Pagination } from "../models";
+
+export class Domain extends RequestService {
+  constructor(apiKey: string, baseUrl: string) {
+    super(apiKey, baseUrl);
+  }
+
+  async list(queryParams?: DomainQueryParams): Promise<APIResponse> {
+    return await this.get(`/domains`, queryParams);
+  }
+
+  async single(domainId: string): Promise<APIResponse> {
+    return await this.get(`/domains/${domainId}`);
+  }
+
+  async recipients(domainId: string, queryParams?: DomainRecipientsQueryParams): Promise<APIResponse> {
+    return await this.get(`/domains/${domainId}/recipients`, queryParams);
+  }
+}
+
+export interface DomainQueryParams extends Pagination {
+  verified?: boolean
+}
+
+export interface DomainRecipientsQueryParams extends Pagination {
+
+}
